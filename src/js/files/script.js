048a7e24e211736167f64487563328543d4ab6f1
khama-web <khama-web@yandex.ru>
2024-05-27 21:46:53 +0500

    added first commit testimonials completed

---
+++ src/js/files/script.js
@@ -0,0 +1,44 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+
+
+// testimonials ========================================================================================================================================================
+// document.addEventListener("DOMContentLoaded", () => {
+//     const swiper = new Swiper('.swiper-2', {
+//         // Optional parameters
+//         direction: 'horizontal',
+//         loop: true,
+//         speed: 400,
+//         autoplay: true,
+//         parallax: true,
+
+//         navigation: {
+//             nextEl: '.swiper-button-next-2',
+//             prevEl: '.swiper-button-prev-2',
+//         },
+//         pagination: {
+//             el: '.swiper-pagination-2',
+//         }
+//     });
+// })
+
+// $('.owl-carousel').owlCarousel({
+//   loop:true,
+//   margin:10,
+//   nav:false,
+//   dots:true,
+//   autoplay:true,
+//   responsive:{
+//       0:{
+//           items:1
+//       },
+//       600:{
+//           items:2
+//       },
+//       1000:{
+//           items:3
+//       }
+//   }
+// })
